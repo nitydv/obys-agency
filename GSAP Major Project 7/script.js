39be95451d30306ab706abd4473163d4f0388a7c
nitydv <nitheshyadav3589@gmail.com>
2025-08-19 20:49:42 +0530

    page with loco complete

--- GSAP Major Project 7/script.js
+++ GSAP Major Project 7/script.js
@@ -1,8 +1,30 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('[data-scroll-container]'),
-//     smooth: true
-// });
+function locomotiveAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
 
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+locomotiveAnimation()
 function loadingAnimation(){
   
   function loaderAnimation(){
@@ -14,13 +36,11 @@ function loadingAnimation(){
       delay: 0.3,
       stagger: 0.4,
     });
-
     gsap.from("#loader h2", {
       y: 100,
       duration: 0.4,
       delay: 1.2,
     });
-
     gsap.from("#loader h4, #loader h5", {
       opacity: 0,
       duration: 1.5,
